ee7515476fceca879f6f9acd52c28bfe1b3158ce
hirantha <mail@hirantha.xyz>
2021-04-03 08:34:58 +0530

    remote get posts

--- app/src/main/java/xyz/hirantha/csskilltest/data/remote/datasources/TypiCodeAPIServiceDataSource.kt
+++ app/src/main/java/xyz/hirantha/csskilltest/data/remote/datasources/TypiCodeAPIServiceDataSource.kt
@@ -1,5 +1,11 @@
 package xyz.hirantha.csskilltest.data.remote.datasources
 
+import androidx.lifecycle.LiveData
+import xyz.hirantha.csskilltest.models.Post
+
 interface TypiCodeAPIServiceDataSource {
+
+    val posts: LiveData<List<Post>>
+
     suspend fun getPosts()
 }
--- app/src/main/java/xyz/hirantha/csskilltest/data/remote/datasources/TypiCodeAPIServiceDataSourceImpl.kt
+++ app/src/main/java/xyz/hirantha/csskilltest/data/remote/datasources/TypiCodeAPIServiceDataSourceImpl.kt
@@ -1,10 +1,29 @@
 package xyz.hirantha.csskilltest.data.remote.datasources
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import xyz.hirantha.csskilltest.data.remote.api.TypiCodeAPIService
+import xyz.hirantha.csskilltest.internal.NoConnectivityException
+import xyz.hirantha.csskilltest.models.Post
 
-class TypiCodeAPIServiceDataSourceImpl(typiCodeAPIService: TypiCodeAPIService) :
+class TypiCodeAPIServiceDataSourceImpl(private val apiService: TypiCodeAPIService) :
     TypiCodeAPIServiceDataSource {
+
+    override val posts: LiveData<List<Post>>
+        get() = _posts
+    private val _posts = MutableLiveData<List<Post>>()
+
     override suspend fun getPosts() {
-        TODO("Not yet implemented")
+        try {
+            val posts = apiService.getPosts().await()
+            _posts.value = posts
+            Log.e("reso", posts.toString())
+        } catch (e: NoConnectivityException) {
+            Log.d("API Service", "no connectivity")
+        } catch (e: Exception) {
+            Log.d("API Service", e.message + "")
+            e.printStackTrace()
+        }
     }
 }
