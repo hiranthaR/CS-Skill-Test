fdf8fe7a8501077820744561a5a53e0452c2c498
hirantha <mail@hirantha.xyz>
2021-04-04 13:49:19 +0530

    toggle theme from settings dome

--- app/src/main/java/xyz/hirantha/csskilltest/ui/settings/SettingFragment.kt
+++ app/src/main/java/xyz/hirantha/csskilltest/ui/settings/SettingFragment.kt
@@ -5,14 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.launch
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 import org.kodein.di.android.x.closestDI
 import org.kodein.di.instance
+import xyz.hirantha.csskilltest.R
 import xyz.hirantha.csskilltest.databinding.FragmentSettingBinding
+import xyz.hirantha.csskilltest.internal.ScopedFragment
 
-class SettingFragment : Fragment(),DIAware {
+class SettingFragment : ScopedFragment(), DIAware {
 
     override val di: DI by closestDI()
     private val viewModelFactory: SettingsViewModelFactory by instance()
@@ -34,7 +38,25 @@ class SettingFragment : Fragment(),DIAware {
         bindUI()
     }
 
-    private fun bindUI() {
-
+    private fun bindUI() = launch {
+        viewModel.theme.await().observe(viewLifecycleOwner, {
+            if (it == null) return@observe
+            binding.progressCircular.visibility = View.GONE
+            binding.btgThemeGroup.visibility = View.VISIBLE
+            val btnId = when (it) {
+                AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM -> R.id.btn_system
+                AppCompatDelegate.MODE_NIGHT_NO -> R.id.btn_light
+                else -> R.id.btn_night
+            }
+            binding.btgThemeGroup.setSingleSelection(btnId)
+            binding.btgThemeGroup.addOnButtonCheckedListener { _, checkedId, _ ->
+                val selectedTheme = when (checkedId) {
+                    R.id.btn_system -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                    R.id.btn_light -> AppCompatDelegate.MODE_NIGHT_NO
+                    else -> AppCompatDelegate.MODE_NIGHT_YES
+                }
+                viewModel.setTheme(selectedTheme)
+            }
+        })
     }
 }
--- app/src/main/java/xyz/hirantha/csskilltest/ui/settings/SettingViewModel.kt
+++ app/src/main/java/xyz/hirantha/csskilltest/ui/settings/SettingViewModel.kt
@@ -2,6 +2,11 @@ package xyz.hirantha.csskilltest.ui.settings
 
 import androidx.lifecycle.ViewModel
 import xyz.hirantha.csskilltest.data.repository.Repository
+import xyz.hirantha.csskilltest.internal.lazyDeferred
 
-class SettingViewModel(private val repository: Repository):ViewModel() {
+class SettingViewModel(private val repository: Repository) : ViewModel() {
+
+    val theme by lazyDeferred { repository.getTheme() }
+
+    fun setTheme(selectedTheme: Int) = repository.setTheme(selectedTheme)
 }
