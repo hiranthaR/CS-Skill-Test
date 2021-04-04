0fa0f0da4e190abe7c0305a3f458ccf1617b0ce7
hirantha <mail@hirantha.xyz>
2021-04-04 06:09:31 +0530

    users model

---
+++ app/src/main/java/xyz/hirantha/csskilltest/models/User.kt
@@ -0,0 +1,25 @@
+package xyz.hirantha.csskilltest.models
+
+
+import androidx.room.Entity
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "users")
+data class User(
+    @SerializedName("address")
+    val address: Address,
+    @SerializedName("company")
+    val company: Company,
+    @SerializedName("email")
+    val email: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("phone")
+    val phone: String,
+    @SerializedName("username")
+    val username: String,
+    @SerializedName("website")
+    val website: String
+)
