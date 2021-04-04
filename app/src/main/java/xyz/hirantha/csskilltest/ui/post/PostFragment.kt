448116be44c9382a17b586f5e0aa1b78fa923c0f
hirantha <mail@hirantha.xyz>
2021-04-04 10:09:54 +0530

    post rendered in post fragment

--- app/src/main/java/xyz/hirantha/csskilltest/ui/post/PostFragment.kt
+++ app/src/main/java/xyz/hirantha/csskilltest/ui/post/PostFragment.kt
@@ -7,12 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import kotlinx.coroutines.launch
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 import org.kodein.di.android.x.closestDI
 import org.kodein.di.factory
 import xyz.hirantha.csskilltest.R
+import xyz.hirantha.csskilltest.databinding.FragmentPostBinding
 import xyz.hirantha.csskilltest.internal.ScopedFragment
 
 
@@ -22,26 +24,35 @@ class PostFragment : ScopedFragment(), DIAware {
     private val viewModelFactory: ((Int) -> PostViewModelFactory) by factory()
     private lateinit var viewModel: PostViewModel
     private val args: PostFragmentArgs by navArgs()
+    private lateinit var binding:FragmentPostBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_post, container, false)
+        binding = FragmentPostBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel =
             ViewModelProvider(this, viewModelFactory(args.postId)).get(PostViewModel::class.java)
-
         bindUI()
     }
 
     private fun bindUI() = launch {
         viewModel.post.await().observe(viewLifecycleOwner,{
             if(it == null) return@observe
-            Log.e("Post",it.toString())
+            binding.tvName.text = it.user.name
+            binding.tvTitle.text = it.post.title
+            binding.tvContent.text = it.post.body
+            Glide.with(requireContext())
+                .load(it.user.avatar)
+                .centerCrop()
+                .placeholder(R.drawable.user)
+                .error(R.drawable.user)
+                .into(binding.imgAvatar)
         })
     }
 }
